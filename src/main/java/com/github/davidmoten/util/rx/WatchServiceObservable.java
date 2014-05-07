5ece2ba4c72898de8c0072ce70fda65f44a6853c
Dave Moten <davidmoten@gmail.com>
2014-05-07 10:03:22 +1000

    dev

--- src/main/java/com/github/davidmoten/util/rx/WatchServiceObservable.java
+++ src/main/java/com/github/davidmoten/util/rx/WatchServiceObservable.java
@@ -2,127 +2,171 @@ package com.github.davidmoten.util.rx;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.ClosedWatchServiceException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.WatchEvent;
 import java.nio.file.WatchEvent.Kind;
 import java.nio.file.WatchKey;
 import java.nio.file.WatchService;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 import rx.Observable;
 import rx.Observable.OnSubscribe;
 import rx.Subscriber;
+import rx.Subscription;
 import rx.functions.Func1;
 
 public class WatchServiceObservable {
 
-	public static Observable<WatchEvent<?>> from(WatchService watchService) {
-		return Observable.create(new WatchServiceOnSubscribe(watchService));
-	}
+    public static Observable<WatchEvent<?>> from(WatchService watchService) {
+        return Observable.create(new WatchServiceOnSubscribe(watchService));
+    }
 
-	public static Observable<WatchEvent<?>> from(final File file,
-			Kind<Path>... kinds) {
-		return watchService(file, kinds).flatMap(TO_WATCH_EVENTS).filter(
-				onlyRelatedTo(file));
-	}
+    @SafeVarargs
+    public static Observable<WatchEvent<?>> from(final File file, Kind<Path>... kinds) {
+        return watchService(file, kinds).flatMap(TO_WATCH_EVENTS).filter(onlyRelatedTo(file));
+    }
 
-	public static Observable<WatchService> watchService(final File file,
-			final Kind<Path>... kinds) {
-		return Observable.create(new OnSubscribe<WatchService>() {
+    @SafeVarargs
+    public static Observable<WatchService> watchService(final File file, final Kind<Path>... kinds) {
+        return Observable.create(new OnSubscribe<WatchService>() {
 
-			@Override
-			public void call(Subscriber<? super WatchService> subscriber) {
-				final Path path;
-				if (file.isDirectory())
-					path = Paths.get(file.toURI());
-				else
-					path = Paths.get(file.getParentFile().toURI());
-				try {
-					System.out.println("creating watch service for " + path);
-					WatchService watchService = path.getFileSystem()
-							.newWatchService();
-					path.register(watchService, kinds);
-					subscriber.onNext(watchService);
-					subscriber.onCompleted();
-				} catch (IOException e) {
-					subscriber.onError(e);
-				}
-			}
-		});
-	}
+            @Override
+            public void call(Subscriber<? super WatchService> subscriber) {
+                final Path path;
+                if (file.isDirectory())
+                    path = Paths.get(file.toURI());
+                else
+                    path = Paths.get(file.getParentFile().toURI());
+                try {
+                    System.out.println("creating watch service for " + path);
+                    WatchService watchService = path.getFileSystem().newWatchService();
+                    path.register(watchService, kinds);
+                    subscriber.onNext(watchService);
+                    subscriber.onCompleted();
+                } catch (IOException e) {
+                    subscriber.onError(e);
+                }
+            }
+        });
+    }
 
-	private static Func1<WatchEvent<?>, Boolean> onlyRelatedTo(final File file) {
-		return new Func1<WatchEvent<?>, Boolean>() {
+    private static Func1<WatchEvent<?>, Boolean> onlyRelatedTo(final File file) {
+        return new Func1<WatchEvent<?>, Boolean>() {
 
-			@Override
-			public Boolean call(WatchEvent<?> event) {
-				System.out.println("event=" + event);
-				if (file.isDirectory())
-					return true;
-				else {
-					Object context = event.context();
-					if (context != null && context instanceof Path) {
-						Path p = (Path) context;
-						return p.toFile().equals(file);
-					} else
-						return false;
-				}
-			}
-		};
-	}
+            @Override
+            public Boolean call(WatchEvent<?> event) {
+                System.out.println("event to filter=" + event);
+                final boolean ok;
+                if (file.isDirectory())
+                    ok = true;
+                else {
+                    Object context = event.context();
+                    if (context != null && context instanceof Path) {
+                        Path p = (Path) context;
+                        ok = p.toFile().getName().equals(file.getName());
+                    } else
+                        ok = false;
+                }
+                System.out.println("is ok=" + ok);
+                return ok;
+            }
+        };
+    }
 
-	public static class WatchServiceOnSubscribe implements
-			OnSubscribe<WatchEvent<?>> {
+    public static class WatchServiceOnSubscribe implements OnSubscribe<WatchEvent<?>> {
 
-		private final WatchService watchService;
+        private final WatchService watchService;
+        private final AtomicBoolean closed = new AtomicBoolean(false);
 
-		public WatchServiceOnSubscribe(WatchService watchService) {
-			this.watchService = watchService;
-		}
+        public WatchServiceOnSubscribe(WatchService watchService) {
+            this.watchService = watchService;
+        }
 
-		@Override
-		public void call(Subscriber<? super WatchEvent<?>> subscriber) {
-			try {
-				if (subscriber.isUnsubscribed())
-					return;
-				// get the first event before looping
-				System.out.println("waiting for event");
-				WatchKey key = watchService.take();
-				System.out.println("key=" + key);
-				while (key != null) {
-					if (subscriber.isUnsubscribed())
-						return;
-					// we have a polled event, now we traverse it and
-					// receive all the states from it
-					if (key != null) {
-						for (WatchEvent<?> event : key.pollEvents()) {
-							if (subscriber.isUnsubscribed())
-								return;
-							else
-								subscriber.onNext(event);
-						}
+        @Override
+        public void call(Subscriber<? super WatchEvent<?>> subscriber) {
 
-						boolean valid = key.reset();
-						if (!valid) {
-							subscriber.onCompleted();
-							return;
-						}
-					}
-					key = watchService.take();
-				}
-			} catch (InterruptedException e) {
-				subscriber.onError(e);
-			}
-		}
+            if (closed.get()) {
+                subscriber.onError(new RuntimeException(
+                        "WatchService closed. You can only subscribe once to a WatchService."));
+                return;
+            }
+            subscriber.add(createSubscriptionToCloseWatchService(watchService, closed));
 
-	}
+            try {
+                if (subscriber.isUnsubscribed())
+                    return;
+                // get the first event before looping
+                System.out.println("waiting for event");
+                WatchKey key;
+                try {
+                    key = watchService.take();
+                } catch (ClosedWatchServiceException e) {
+                    // must have unsubscribed
+                    return;
+                }
+                System.out.println("key=" + key);
+                while (key != null) {
+                    if (subscriber.isUnsubscribed())
+                        return;
+                    // we have a polled event, now we traverse it and
+                    // receive all the states from it
+                    if (key != null) {
+                        for (WatchEvent<?> event : key.pollEvents()) {
+                            if (subscriber.isUnsubscribed())
+                                return;
+                            else
+                                subscriber.onNext(event);
+                        }
 
-	private static Func1<WatchService, Observable<WatchEvent<?>>> TO_WATCH_EVENTS = new Func1<WatchService, Observable<WatchEvent<?>>>() {
+                        boolean valid = key.reset();
+                        if (!valid) {
+                            subscriber.onCompleted();
+                            return;
+                        }
+                    }
+                    try {
+                        key = watchService.take();
+                    } catch (ClosedWatchServiceException e) {
+                        // must have unsubscribed
+                        return;
+                    }
+                }
+            } catch (InterruptedException e) {
+                // ignore
+            }
+        }
 
-		@Override
-		public Observable<WatchEvent<?>> call(WatchService watchService) {
-			return from(watchService);
-		}
-	};
+    }
+
+    private static Subscription createSubscriptionToCloseWatchService(
+            final WatchService watchService, final AtomicBoolean closed) {
+        return new Subscription() {
+
+            @Override
+            public void unsubscribe() {
+                try {
+                    watchService.close();
+                    closed.set(true);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+
+            @Override
+            public boolean isUnsubscribed() {
+                return closed.get();
+            }
+        };
+    }
+
+    private static Func1<WatchService, Observable<WatchEvent<?>>> TO_WATCH_EVENTS = new Func1<WatchService, Observable<WatchEvent<?>>>() {
+
+        @Override
+        public Observable<WatchEvent<?>> call(WatchService watchService) {
+            return from(watchService);
+        }
+    };
 
 }
--- src/test/java/com/github/davidmoten/util/rx/WatchServiceObservableTest.java
+++ src/test/java/com/github/davidmoten/util/rx/WatchServiceObservableTest.java
@@ -1,6 +1,5 @@
 package com.github.davidmoten.util.rx;
 
-import static java.nio.file.StandardWatchEventKinds.ENTRY_CREATE;
 import static java.nio.file.StandardWatchEventKinds.ENTRY_MODIFY;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
@@ -8,11 +7,16 @@ import static org.junit.Assert.assertTrue;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.file.StandardWatchEventKinds;
 import java.nio.file.WatchEvent;
+import java.nio.file.WatchEvent.Kind;
+import java.util.List;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 
 import org.junit.Test;
+import org.mockito.InOrder;
+import org.mockito.Mockito;
 
 import rx.Observable;
 import rx.Observer;
@@ -21,69 +25,71 @@ import rx.schedulers.Schedulers;
 
 public class WatchServiceObservableTest {
 
-	@Test
-	public void testNoEventsThrownIfFileDoesNotExist()
-			throws InterruptedException {
-		File file = new File("target/does-not-exist");
-		@SuppressWarnings("unchecked")
-		Observable<WatchEvent<?>> events = WatchServiceObservable.from(file,
-				ENTRY_MODIFY);
-		final CountDownLatch latch = new CountDownLatch(1);
-		Subscription sub = events.subscribeOn(Schedulers.io()).subscribe(
-				new Observer<WatchEvent<?>>() {
+    @Test
+    public void testNoEventsThrownIfFileDoesNotExist() throws InterruptedException {
+        File file = new File("target/does-not-exist");
+        @SuppressWarnings("unchecked")
+        Observable<WatchEvent<?>> events = WatchServiceObservable.from(file, ENTRY_MODIFY);
+        final CountDownLatch latch = new CountDownLatch(1);
+        Subscription sub = events.subscribeOn(Schedulers.io()).subscribe(
+                new Observer<WatchEvent<?>>() {
 
-					@Override
-					public void onCompleted() {
-						latch.countDown();
-					}
+                    @Override
+                    public void onCompleted() {
+                        latch.countDown();
+                    }
 
-					@Override
-					public void onError(Throwable arg0) {
-						latch.countDown();
-					}
+                    @Override
+                    public void onError(Throwable arg0) {
+                        latch.countDown();
+                    }
 
-					@Override
-					public void onNext(WatchEvent<?> arg0) {
-						latch.countDown();
-					}
-				});
-		assertFalse(latch.await(100, TimeUnit.MILLISECONDS));
-		sub.unsubscribe();
-	}
+                    @Override
+                    public void onNext(WatchEvent<?> arg0) {
+                        latch.countDown();
+                    }
+                });
+        assertFalse(latch.await(100, TimeUnit.MILLISECONDS));
+        sub.unsubscribe();
+    }
 
-	@Test
-	public void testEvents() throws InterruptedException, IOException {
-		File file = new File("target/f");
-		file.delete();
-		@SuppressWarnings("unchecked")
-		Observable<WatchEvent<?>> events = WatchServiceObservable.from(file,
-				ENTRY_CREATE, ENTRY_MODIFY);
-		final CountDownLatch latch = new CountDownLatch(1);
-		Subscription sub = events.subscribeOn(Schedulers.io()).subscribe(
-				new Observer<WatchEvent<?>>() {
+    @Test
+    public void testEvents() throws InterruptedException, IOException {
+        File file = new File("target/f");
+        file.delete();
+        file.createNewFile();
+        @SuppressWarnings("unchecked")
+        Observable<WatchEvent<?>> events = WatchServiceObservable.from(file, ENTRY_MODIFY);
+        final CountDownLatch latch = new CountDownLatch(1);
+        final List<Kind> eventKinds = Mockito.mock(List.class);
+        InOrder inOrder = Mockito.inOrder(eventKinds);
+        Subscription sub = events.subscribeOn(Schedulers.io()).subscribe(
+                new Observer<WatchEvent<?>>() {
 
-					@Override
-					public void onCompleted() {
-						System.out.println("completed");
-					}
+                    @Override
+                    public void onCompleted() {
+                        System.out.println("completed");
+                    }
 
-					@Override
-					public void onError(Throwable e) {
-						e.printStackTrace();
-					}
+                    @Override
+                    public void onError(Throwable e) {
+                        e.printStackTrace();
+                    }
 
-					@Override
-					public void onNext(WatchEvent<?> event) {
-						System.out.println("event=" + event);
-						latch.countDown();
-					}
-				});
-
-		file.createNewFile();
-		FileOutputStream fos = new FileOutputStream(file, true);
-		fos.write("hello there".getBytes());
-		fos.close();
-		assertTrue(latch.await(1000, TimeUnit.MILLISECONDS));
-		sub.unsubscribe();
-	}
+                    @Override
+                    public void onNext(WatchEvent<?> event) {
+                        System.out.println("event=" + event);
+                        eventKinds.add(event.kind());
+                        latch.countDown();
+                    }
+                });
+        Thread.sleep(200);
+        FileOutputStream fos = new FileOutputStream(file, true);
+        fos.write("hello there".getBytes());
+        fos.close();
+        assertTrue(latch.await(30000, TimeUnit.MILLISECONDS));
+        inOrder.verify(eventKinds).add(StandardWatchEventKinds.ENTRY_MODIFY);
+        inOrder.verifyNoMoreInteractions();
+        sub.unsubscribe();
+    }
 }
