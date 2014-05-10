2703a33892d7afacd2c0e6771d6e378472170f8c
Dave Moten <davidmoten@gmail.com>
2014-05-10 13:53:32 +1000

    add comment

--- src/main/java/com/github/davidmoten/util/rx/FileObservable.java
+++ src/main/java/com/github/davidmoten/util/rx/FileObservable.java
@@ -191,11 +191,14 @@ public class FileObservable {
 			} catch (InterruptedException e) {
 				// this case is problematic because unsubscribe may call
 				// Thread.interrupt() before calling the unsubscribe method of
-				// the Subscription. Thus at this point I don't know if a
+				// the Subscription. Thus at this point we don't know if a
 				// deliberate interrupt was called in which case I would call
 				// onComplete or if unsubscribe was called in which case I
-				// should not call anything. I prefer to take the more
-				// conservative approach and not call anything.
+				// should not call anything. For the moment I choose to not call
+				// anything partly because a deliberate stop of the
+				// watchService.take ignorant of the Observable should ideally
+				// happen via a call to the WatchService.close() method rather
+				// than Thread.interrupt().
 				// TODO raise the issue with RxJava team in particular
 				// Subscriptions.from(Future) calling FutureTask.cancel(true)
 				try {
